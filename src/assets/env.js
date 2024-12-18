3dcadd253a03abd41bf3d2e0dfe663932c332c0a
Foday Saccoh <171730704+fodayba@users.noreply.github.com>
2024-12-18 13:36:38 +0000

    fixed commit

--- src/assets/env.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    window.env = window.env || {};
-    // Environment variables
-    window.env.publicKey = '${publicKey}';
-    window.env.serviceId = '${serviceId}';
-    window.env.templateId = '${templateId}';
-    window.env.toEmail = '${toEmail}';
-})(this); 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,9 @@
-declare global {
-  interface Window {
-    env: {
-      publicKey: string;
-      serviceId: string;
-      templateId: string;
-      toEmail: string;
-    };
-  }
-}
-
 export const environment = {
   production: false,
   emailJs: {
-    publicKey: typeof window !== 'undefined' ? window.env?.publicKey : process.env['publicKey'] || '',
-    serviceId: typeof window !== 'undefined' ? window.env?.serviceId : process.env['serviceId'] || '',
-    templateId: typeof window !== 'undefined' ? window.env?.templateId : process.env['templateId'] || '',
-    toEmail: typeof window !== 'undefined' ? window.env?.toEmail : process.env['toEmail'] || ''
+    publicKey: process.env['publicKey'] || '',
+    serviceId: process.env['serviceId'] || '',
+    templateId: process.env['templateId'] || '',
+    toEmail: process.env['toEmail'] || ''
   }
 }; 
